60daa12432aee8ccb4e1bc3940e673b3d2e72faa
samin2668 <44666180+samin2668@users.noreply.github.com>
2023-08-07 14:21:54 -0500

    initial commit

---
+++ my-portfolio/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <>
+      <header className="text-5xl m-8">
+        Samin Yasar
+      </header>
+      <div className="m-16">
+        Experience
+      </div>
+    </>
+  )
+}
